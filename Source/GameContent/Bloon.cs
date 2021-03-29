b3bcc75b309a08ff81ba882b4e9a5b0ae6c92fd2
Stevie <xxlennygamerxx@gmail.com>
2021-03-28 22:14:53 -0700

    I forgot virtual properties literally work the same

--- Source/GameContent/Bloon.cs
+++ Source/GameContent/Bloon.cs
@@ -8,6 +8,21 @@ namespace BTDMG.Source.GameContent
     public abstract class Bloon
     {
         /// <summary>
+        ///     The path the <see cref="Bloon" /> is on.
+        /// </summary>
+        public BloonPath Path { get; set; }
+
+        /// <summary>
+        ///     The track the <see cref="Bloon" /> is on.
+        /// </summary>
+        public virtual BloonTrack Track { get; set; }
+
+        /// <summary>
+        ///     The <see cref="Bloon" />'s progress along its <see cref="Path" />.
+        /// </summary>
+        public virtual float Progress { get; set; }
+
+        /// <summary>
         ///     Normally calls <see cref="BloonTrack.GetPositionFromPath{T}" />, but can be overridden to whatever you want.
         /// </summary>
         /// <returns></returns>
@@ -17,110 +32,5 @@ namespace BTDMG.Source.GameContent
         ///     Called when a bloon escapes a path.
         /// </summary>
         public virtual void OnEscape() { }
-
-        #region Path Stuff
-
-        /// <summary>
-        ///     The <see cref="Bloon" />'s associated path. <br />
-        ///     This should never be used outside of <see cref="GetPath" /> and <see cref="SetPath" />, instead, use
-        ///     <see cref="Path" />.
-        /// </summary>
-        private BloonPath _path;
-
-        /// <summary>
-        ///     The path the <see cref="Bloon" /> is on.
-        /// </summary>
-        public BloonPath Path
-        {
-            get => GetPath();
-
-            set => SetPath(value);
-        }
-
-        /// <summary>
-        ///     Allows you to run any code or intercept <see cref="Path" />'s getter, allowing you to decide whether or not it
-        ///     should return <see cref="_path" /> or your own, separate <see cref="BloonPath" />.
-        /// </summary>
-        /// <returns></returns>
-        protected virtual BloonPath GetPath() => _path;
-
-        /// <summary>
-        ///     Allows you to run any code or intercept <see cref="Path" />'s setter, allowing you to decide whether or not
-        ///     <see cref="_path" /> should be changed.
-        /// </summary>
-        /// <param name="path">The path that <see cref="_path" /> is trying to be set to.</param>
-        protected virtual void SetPath(BloonPath path) => _path = path;
-
-        #endregion
-
-        #region Track Stuff
-
-        /// <summary>
-        ///     The <see cref="Bloon" />'s associated track.
-        ///     This should never be used outside of <see cref="GetTrack" /> and <see cref="SetTrack" />, instead, use
-        ///     <see cref="Track" />.
-        /// </summary>
-        private BloonTrack _track;
-
-        /// <summary>
-        ///     The track the <see cref="Bloon" /> is on.
-        /// </summary>
-        public BloonTrack Track
-        {
-            get => GetTrack();
-
-            set => SetTrack(value);
-        }
-
-        /// <summary>
-        ///     Allows you to run any code or intercept <see cref="Track" />'s getter, allowing you to decide whether or not it
-        ///     should return <see cref="_track" /> or your own, separate <see cref="BloonTrack" />.
-        /// </summary>
-        /// <returns></returns>
-        protected virtual BloonTrack GetTrack() => _track;
-
-        /// <summary>
-        ///     Allows you to run any code or intercept <see cref="Track" />'s setter, allowing you to decide whether or not
-        ///     <see cref="_track" /> should be changed.
-        /// </summary>
-        /// <param name="track">The track that <see cref="_track" /> is trying to be set to.</param>
-        protected virtual void SetTrack(BloonTrack track) => _track = track;
-
-        #endregion
-
-        #region Progress Stuff
-
-        /// <summary>
-        ///     The <see cref="Bloon" />'s progress along its <see cref="Path" />.
-        ///     This should never be used outside of <see cref="GetProgress" /> and <see cref="SetProgress" />, instead, use
-        ///     <see cref="Progress" />.
-        /// </summary>
-        private float _progress;
-
-        /// <summary>
-        ///     The <see cref="Bloon" />'s progress along its <see cref="Path" />.
-        /// </summary>
-        public float Progress
-        {
-            get => GetProgress();
-
-            set => SetProgress(value);
-        }
-
-        /// <summary>
-        ///     Allows you to run any code or intercept <see cref="Progress" />' getter, allowing you to decide whether or not it
-        ///     should return <see cref="_progress" /> or your own, separate <see cref="float" />.
-        /// </summary>
-        /// <returns></returns>
-        protected virtual float GetProgress() => _progress;
-
-        /// <summary>
-        ///     Allows you to run any code or intercept <see cref="Progress" />' setter, allowing you to decide whether or not
-        ///     <see cref="_progress" /> should be changed.
-        /// </summary>
-        /// <param name="progress">The amount of progress that <see cref="_progress" /> is trying to be set to.</param>
-        protected virtual void SetProgress(float progress) => _progress = progress;
-
-        #endregion
     }
 }
